83016bfd3a5277358a4de575b90a27328fa2ccfd
Mikołaj <mikolajkocik@onet.pl>
2025-06-06 00:38:37 +0200

    dbcontext init (without conf and migration)

---
+++ BookStoreApi/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookStoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BookStoreApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);         
+        }
+    }
+}
