f2bfc89b8dabd9f9cd9eab7d1e2f75934445eb4a
Mikołaj Kocik <mikolajkocik@onet.pl>
2025-06-08 23:12:27 +0200

    getBooks queryable

--- BookStoreApi/Slices/Books/GetBooks/GetBooksEndpoint.cs
+++ BookStoreApi/Slices/Books/GetBooks/GetBooksEndpoint.cs
@@ -7,10 +7,42 @@ namespace BookStoreApi.Slices.Books.GetBooks
     {
         public static void MapGetBooksEndpoint(this WebApplication app)
         {
-            app.MapGet("/api/books", async (int page, int pageSize, AppDbContext context) =>
+            app.MapGet("/api/books", async (
+                int page,
+                int pageSize,
+                string? author,
+                string? title,
+                decimal? minPrice,
+                decimal? maxPrice,
+                AppDbContext context) =>
             {
+
                 if (page <= 0) page = 1;
                 if (pageSize <= 0) pageSize = 10;
+
+                var query = context.Books.AsQueryable();
+
+                // filtration
+
+                if(!string.IsNullOrWhiteSpace(author))
+                {
+                    query.Where(b => b.Author.Contains(author));
+                }
+                
+                if(!string.IsNullOrWhiteSpace(title))
+                {
+                    query.Where(b => b.Title.Contains(title));
+                }
+
+                if(minPrice.HasValue)
+                {
+                    query.Where(b => b.Price >= minPrice.Value);
+                }
+
+                if(maxPrice.HasValue)
+                {
+                    query.Where(b => b.Price <= maxPrice.Value);
+                }
 
                 // count books
                 var totalCount = context.Books.CountAsync();
